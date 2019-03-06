28be9d3f7a7a228c1dec2940c495f0ebd223fed6
Samuel Sieb <samuel@sieb.net>
2019-03-06 11:39:08 -0800

    download and cache households

--- support.py
+++ support.py
@@ -7,7 +7,7 @@ from api import doGet, doPost
 F1PID = "245145"  # field ID for the Fellowship 1 personal ID
 F1HID = "245146"  # field ID for the Fellowship 1 household ID
 
-loaded = False
+pLoaded = False
 people = {}
 iMap = {}
 
@@ -20,15 +20,15 @@ The data is pickled to "people.pkl" and loaded from there if it exists
 To refresh the data, delete the "people.pkl" file.
 """
 def getPeople(onlyActive=False):
-  global loaded, people, iMap
-  if loaded:
+  global pLoaded, people, iMap
+  if pLoaded:
     return (people, iMap)
   if Path('people.pkl').is_file():
     print("read pickled people")
     with open('people.pkl', 'rb') as pkl:
       people = pickle.load(pkl)
       iMap = pickle.load(pkl)
-    loaded = True
+    pLoaded = True
     return (people, iMap)
   print("fetch people")
   people = {}
@@ -42,7 +42,7 @@ def getPeople(onlyActive=False):
     data = doGet(url)
     total = int(data["meta"]["total_count"])
     count += int(data["meta"]["count"])
-    print(f"{count}/{total} people")
+    print(f"{count}/{total} people\r", end="", flush=True)
     for person in data["data"]:
       people[person["id"]] = [person["attributes"]["name"], None, None]
     for field in data["included"]:
@@ -65,6 +65,46 @@ def getPeople(onlyActive=False):
   with open('people.pkl', 'wb') as pkl:
     pickle.dump(people, pkl)
     pickle.dump(iMap, pkl)
-  loaded = True
+  pLoaded = True
   return (people, iMap)
+
+
+hLoaded = False
+households = {}
+
+"""
+Returns a dictionary mapping the PCO household ID to a list of the members' person IDs
+The data is pickled to "households.pkl" and loaded from there if it exists
+To refresh the data, delete the "households.pkl" file.
+"""
+def getHouseholds():
+  global hLoaded, households
+  if hLoaded:
+    return households
+  if Path('households.pkl').is_file():
+    print("read pickled households")
+    with open('households.pkl', 'rb') as pkl:
+      households = pickle.load(pkl)
+    hLoaded = True
+    return households
+  print("fetch households")
+  chunk = 0
+  total = 1000
+  while chunk < total:
+    data = doGet(f'https://api.planningcenteronline.com/people/v2/households?include=people&per_page=100&offset={chunk}')
+    houseList = []
+    total = int(data["meta"]["total_count"])
+    chunk += int(data["meta"]["count"])
+    for house in data["data"]:
+      hid = house["id"]
+      print(f"{chunk}/{total} households\r", end="", flush=True)
+      members = []
+      for person in house["relationships"]["people"]["data"]:
+        members.append(person["id"])
+      households[hid] = members
+  print("write pickled households")
+  with open('households.pkl', 'wb') as pkl:
+    pickle.dump(households, pkl)
+  hLoaded = True
+  return households
 
