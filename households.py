cba1e2c9a9a12660fde0d7285c2fd5e8582306cb
Samuel Sieb <samuel@sieb.net>
2019-03-25 11:13:53 -0700

    find people in multiple households remove any from that list that have an individual household

---
+++ households.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+# Find people that have multiple households and delete their individual household
+
+from api import doGet, doDelete
+from support import getPeople, getHouseholds
+
+people, iMap = getPeople()
+households = getHouseholds()
+
+pMap = {}
+for hid, members in households.items():
+  for iid in members:
+    if iid not in pMap:
+      pMap[iid] = []
+    pMap[iid].append(hid)
+count = 0
+toRemove = set()
+for iid, houses in pMap.items():
+  if len(houses) > 1:
+    count += 1
+    s = f"{people[iid][0]} has {len(houses)} households:"
+    for hid in houses:
+      num = len(households[hid])
+      if num == 1:
+        toRemove.add(hid)
+      s += f" {hid}({num})"
+    print(s)
+print(f"{count} multiples, removing {len(toRemove)}")
+total = len(toRemove)
+for i, hid in enumerate(toRemove):
+  print(f"Deleting {i+1}/{total} - {hid}               \r", end="", flush=True)
+  doDelete(f"https://api.planningcenteronline.com/people/v2/households/{hid}")
+print()
+
